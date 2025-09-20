e38f6a720d2e28ad0bbff0f772a28a87e6ff2bc0
ksaddo <THE_EMAIL_FROM_GITHUB_SETTINGS>
2025-09-20 14:52:13 +0000

    vercel error fixed

--- app/wishlist/page.js
+++ app/wishlist/page.js
@@ -6,8 +6,8 @@ export default function WishListPage() {
     <div className="flex flex-col items-center space-y-6 h-full text-center px-4 mt-32">
       <h3 className="text-3xl font-bold">Wishlist is empty</h3>
       <p>
-        You don't have any products in the wishlist yet. You will find a lot of
-        interesting products on our "Shop" page.
+        You don&apos;t have any products in the wishlist yet. You will find a
+        lot of interesting products on our &quot;Shop&quot; page.
       </p>
       <Link
         href="/products"
